f1b5333067a213c819d3e8d7fa857c1ec0d8cc9f
jordaninsfran <jordan.insfran@uner.edu.ar>
2026-08-10 10:07:56 -0300

    Se añaden ejemplos polimorifismo

---
+++ conceptos/unidad_02/ejemplo_polimorfismo_orquesta_v1_duck_typing.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+Ejemplo de polimorfismo sin jerarquía (Duck Typing).
+"""
+
+class Guitarra:
+    def ejecutar(self):
+        print("ejecutar nota guitarra")
+
+class Arpa:
+    def ejecutar(self):
+        print("ejecutar nota arpa")
+
+class Flauta:
+    def ejecutar(self):
+        # código específico para ejecutar una flauta
+        c = 4 # una variable cualquiera que solo existe en el contexto del método ejecutar de la clase Flauta
+        print("ejecutar nota flauta")
+
+
+class Orquesta:
+    def __init__(self) -> None:
+        self.__instrumentos = []
+    
+    def agregar(self, p_instrumento):
+        no_pertenece_aun = p_instrumento not in self.__instrumentos
+        if no_pertenece_aun:
+            self.__instrumentos.append(p_instrumento)
+        else:
+            raise Exception("O no es subclase, o ya pertenece a la orquesta.")
+
+    def ejecutar_obra(self):
+        for instrumento in self.__instrumentos:
+            instrumento.ejecutar()
+
+    def ejecutar_obra_de_instrumentos_afinables(self):
+        for instrumento in self.__instrumentos:
+            if isinstance(instrumento, Guitarra) or isinstance(instrumento, Arpa):
+                instrumento.ejecutar()
+
+
+class MartilloNeumatico:
+    def ejecutar(self):
+        print("ejecutar máquina: martillo neumático")
+
+
+if __name__ == "__main__":
+    # f1 = Flauta()
+
+    # f1.ejecutar()
+
+    o = Orquesta()
+
+    o.agregar(Flauta())
+    o.agregar(Flauta())
+    o.agregar(Guitarra())
+    o.agregar(Arpa())
+
+    # o.agregar(MartilloNeumatico()) # ¡¿What?!
+
+    print("Inicia la obra:")
+    o.ejecutar_obra()
+
+    # v = Viento()  # No se puede, porque Viento es clase abstracta
+
+    print()
+    print("Inicia la obra con instrumentos afinables:")
+    o.ejecutar_obra_de_instrumentos_afinables()
+
+    
